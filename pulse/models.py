59ebf07d157c263ae8791c1a239018781a517e83
vikram <thir20ad050@rmkcet.ac.in>
2023-07-16 19:02:34 +0530

    preloader added and scroll down effect added...

--- pulse/models.py
+++ pulse/models.py
@@ -7,3 +7,35 @@ class user_details(models.Model):
     password=models.CharField(max_length=50)
     def __str__(self):
         return self.username
+
+
+class chat_message_100(models.Model):
+    username=models.CharField(max_length=50)
+    chat_type=models.CharField(max_length=50)
+    message_type=models.CharField(max_length=50)
+    img_url=models.FileField(default=None)
+    message=models.CharField(max_length=1000)
+
+
+    def __str__(self):
+        return self.username
+
+
+class chat_message_101(models.Model):
+    username = models.CharField(max_length=50)
+    chat_type = models.CharField(max_length=50)
+    message_type = models.CharField(max_length=50)
+    img_url = models.FileField(default=None)
+    message = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.username
+
+
+class chat_message_200(models.Model):
+    username = models.CharField(max_length=50)
+    chat_type = models.CharField(max_length=50)
+    message_type = models.CharField(max_length=50)
+    img_url = models.FileField(default=None)
+    message = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.username
